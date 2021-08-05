08def5013dbf2444273964c44f1baf4363cda552
Carlos Rodriguez <crodriguez3753@gmail.com>
2021-08-05 17:39:07 -0400

    set up .js files

---
+++ index.js
@@ -0,0 +1,36 @@
+const questions = require("./utils/questions");
+const inquirer = require("inquirer");
+const Employee = require("./lib/Employee");
+
+async function main(){
+    
+    // htmlCardArray = [];
+
+    // generate manager
+        // ask questions 
+        // render new manger based on object props 
+        // push to htmlCardArray
+    
+    // generate manager
+        // ask questions 
+        // render new manger based on object props 
+        // push to htmlCardArray
+    
+    // generate manager
+        // ask questions 
+        // render new manger based on object props 
+        // push to htmlCardArray
+
+    // mainMenu func
+        // generate Engineer
+        // generate Intern
+        // bye
+
+
+
+
+
+}
+
+// Start 🚀
+main();
---
+++ utils/questions.js
@@ -0,0 +1,15 @@
+module.exports = [
+
+    {
+        message: "what is your name",
+        name: "name",
+    },
+    {
+        message: "what is your id",
+        name: "id",
+    },
+    {
+        message: "what is your email",
+        name: "email",
+    },
+]
